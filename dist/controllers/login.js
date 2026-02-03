7407c5c5cba674a51d7783841c027f0904fb5a81
vedapatkar04 <patkarvedanti@gmail.com>
2026-02-03 22:00:48 +0530

    docker

--- dist/controllers/login.js
+++ dist/controllers/login.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = void 0;
+exports.login = login;
 const models_1 = require("../models");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
@@ -33,4 +33,3 @@ async function login(req, res) {
         res.status(500).json({ message: "Server error" });
     }
 }
-exports.login = login;
--- dist/controllers/register.js
+++ dist/controllers/register.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = void 0;
+exports.register = register;
 const models_1 = require("../models");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
@@ -40,4 +40,3 @@ async function register(req, res) {
         res.status(500).json({ message: "Server error" });
     }
 }
-exports.register = register;
